a634432cc7c6652620484432d303cf43e383ec2d
unknown <olaolushina55@gmail.com>
2023-08-25 14:57:10 +0100

    just added page router and active links

--- js/script.js
+++ js/script.js
@@ -2,26 +2,37 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    if (link.getAttribute("href") === global.currentPage) {
+      link.classList.add("active");
+    }
+  });
+}
+
 // Init App
 function init() {
   switch (global.currentPage) {
-    case '/':
-    case '/index.html': 
-      console.log('Home');
+    case "/":
+    case "/index.html":
+      console.log("Home");
       break;
-    case '/shows.html':
-    console.log('Shows');
-    break;
-    case ' /movie-details.html':
-    console.log('Movie Detaails');
-    break;
-    case ' /tv-details.html':
-    console.log('Tv Detaails');
-    break;
-    case ' /search.html':
-    console.log('Search');
-    break;
+    case "/shows.html":
+      console.log("Shows");
+      break;
+    case "/movie-details.html":
+      console.log("Movie Details");
+      break;
+    case "/tv-details.html":
+      console.log("TV Details");
+      break;
+    case "/search.html":
+      console.log("Search Details");
+      break;
   }
+  highlightActiveLink();
 }
 
 document.addEventListener("DOMContentLoaded", init);
